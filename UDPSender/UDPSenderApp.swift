24507d06fbd01974fa9766cefc587a898e28aac7
Haochen Ji <462499571@qq.com>
2023-07-06 13:36:08 +0800

    Initial Commit

---
+++ UDPSender/UDPSenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UDPSenderApp.swift
+//  UDPSender
+//
+//  Created by Justin on 7/6/23.
+//
+
+import SwiftUI
+
+@main
+struct UDPSenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
